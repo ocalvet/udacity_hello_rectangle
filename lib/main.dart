98644fbfe743acd368f8250ebe99c553b488937d
Ovidio R. Calvet <calveto@gmail.com>
2018-08-14 23:03:51 -0400

    Switch to a statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -17,30 +17,40 @@ void main() {
   );
 }
 
-class HelloRectangle extends StatelessWidget {
+class HelloRectangle extends StatefulWidget {
+  HelloRectangleState createState() => HelloRectangleState();
+}
+
+class HelloRectangleState extends State<HelloRectangle> {
+  Random random = new Random();
+  Color selectedColor;
   Color getRandomColor() {
-    Random random = new Random();
-    return Color.fromARGB(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    return Color.fromARGB(random.nextInt(255), random.nextInt(255),
+        random.nextInt(255), random.nextInt(255));
   }
+
   @override
   Widget build(BuildContext context) {
     return Center(
         child: GestureDetector(
-      onTap: () {
-        print('Changing color');
-      },
-      child: Container(
-        color: getRandomColor(),
-        height: 400.0,
-        width: 300.0,
-        child: Center(
-          child: Text(
-            'Hello!',
-            style: TextStyle(fontSize: 40.0),
-            textAlign: TextAlign.center,
+          onTap: () {
+            print('Changing color');
+            setState(() {
+              selectedColor = getRandomColor();
+            });
+          },
+          child: Container(
+            color: selectedColor,
+            height: 400.0,
+            width: 300.0,
+            child: Center(
+              child: Text(
+                'Hello!',
+                style: TextStyle(fontSize: 40.0),
+                textAlign: TextAlign.center,
+              ),
+            ),
           ),
-        ),
-      ),
     ));
   }
 }
